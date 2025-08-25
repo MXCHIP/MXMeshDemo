df9c93ce77689be77b37f702a044ee35125ec0c4
huafeng <zhanghuafeng0218@163.com>
2025-08-25 14:45:06 +0800

    删除不需要的库

--- MXFrameworks/MXHAC/libHAC.h
+++
@@ -1,9 +0,0 @@
-void HAC_Init();
-int HAC_Connect(const char *addr, int port, const char *mac, const char *password);
-int HAC_Get(const char *path, const char *args);
-int HAC_Put(const char *path, const char *body);
-int HAC_GetStatus();
-const char *HAC_GetBody();
-void HAC_Disconnect();
-void HAC_SetOnDisconnect(void (*callback)());
-void HAC_SetOnEvent(void (*callback)(const char *event));
